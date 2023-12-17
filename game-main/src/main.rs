528351fa9103c5619387f665c0aa28405d917179
Jeff Schwab <jeff@wieldy.ai>
2023-12-17 08:30:10 -0500

    Try to hide the cursor, but it crashes

--- game-main/src/main.rs
+++ game-main/src/main.rs
@@ -6,24 +6,37 @@ mod plugins;
 
 #[cfg(feature = "debug")]
 use crate::plugins::DebugPlugin;
-use crate::plugins::{MovementPlugin, SpaceshipPlugin};
+use crate::plugins::{
+    AssetLoaderPlugin, AsteroidPlugin, CollisionDetectionPlugin, DespawnPlugin, MovementPlugin,
+    SpaceshipPlugin,
+};
 
 use game_camera::CameraPlugin;
 
 use bevy::prelude::*;
-use plugins::{AssetLoaderPlugin, AsteroidPlugin, CollisionDetectionPlugin, DespawnPlugin};
+use bevy::window::{PrimaryWindow, Window};
+
+pub fn hide_cursor(mut window: Query<&mut Window, With<PrimaryWindow>>) {
+    let Ok(mut window) = window.get_single_mut() else {
+        return;
+    };
+    window.cursor.visible = false;
+}
 
 fn main() {
     let mut app = App::new();
-    let app = app.add_plugins(DefaultPlugins).add_plugins((
-        AssetLoaderPlugin,
-        AsteroidPlugin,
-        CameraPlugin,
-        CollisionDetectionPlugin,
-        DespawnPlugin,
-        MovementPlugin,
-        SpaceshipPlugin,
-    ));
+    let app = app
+        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            AssetLoaderPlugin,
+            AsteroidPlugin,
+            CameraPlugin,
+            CollisionDetectionPlugin,
+            DespawnPlugin,
+            MovementPlugin,
+            SpaceshipPlugin,
+        ))
+        .add_systems(PostStartup, hide_cursor);
 
     #[cfg(feature = "debug")]
     app.add_plugins(DebugPlugin);
